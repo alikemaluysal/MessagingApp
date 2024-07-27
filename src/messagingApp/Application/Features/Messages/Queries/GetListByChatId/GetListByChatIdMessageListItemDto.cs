5fe78af23efd6b9bb58a88325da2aef265f4c605
Ali Kemal Uysal <alikemaluysal4235@gmail.com>
2024-07-27 12:57:46 +0300

    feat: get messages by chat id

---
+++ src/messagingApp/Application/Features/Messages/Queries/GetListByChatId/GetListByChatIdMessageListItemDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Features.Messages.Queries.GetListByChatId;
+
+public class GetListByChatIdMessageListItemDto
+{
+    public Guid Id { get; set; }
+    public Guid ChatId { get; set; }
+    public Guid UserId { get; set; }
+    public string? Content { get; set; }
+    public string? FileIdentifier { get; set; }
+    public DateTime SentAt { get; set; }
+    public string SenderName { get; set; } = default!;
+
+}
---
+++ src/messagingApp/Application/Features/Messages/Queries/GetListByChatId/GetListByChatIdMessageQuery.cs
@@ -0,0 +1,37 @@
+﻿using Application.Features.Messages.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Messages.Queries.GetListByChatId;
+
+public class GetListByChatIdMessageQuery : IRequest<List<GetListByChatIdMessageListItemDto>>
+{
+    public Guid ChatId { get; set; }
+
+    class GetListByChatIdMessageQueryHandler(
+        IMessageRepository messageRepository,
+        IChatRepository chatRepository,
+        IMapper mapper,
+        MessageBusinessRules messageBusinessRules
+        
+        ) : IRequestHandler<GetListByChatIdMessageQuery, List<GetListByChatIdMessageListItemDto>>
+    {
+
+        public async Task<List<GetListByChatIdMessageListItemDto>> Handle(GetListByChatIdMessageQuery request, CancellationToken cancellationToken)
+        {
+
+            var chat = await chatRepository.GetAsync(c => c.Id == request.ChatId);
+            messageBusinessRules.ChatShouldExistWhenSelected(chat);
+
+            var messages = await messageRepository.GetListAsync(
+                predicate: m => m.ChatId == request.ChatId,
+                include: m => m.Include(m => m.User),
+                orderBy: m => m.OrderBy(m => m.SentAt)
+                );
+
+            return mapper.Map<List<GetListByChatIdMessageListItemDto>>(messages);
+        }
+    }
+}
