3b9c6a05eeef9346f04a51a63a20041a38dcca7b
Ali Kemal Uysal <alikemaluysal4235@gmail.com>
2024-07-27 11:04:04 +0300

    feat: join chat command implemented

---
+++ src/messagingApp/Application/Features/Chats/Constants/ErrorMessages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Chats.Constants;
+
+internal class ErrorMessages
+{
+}
--- src/messagingApp/Application/Features/Chats/Rules/ChatBusinessRules.cs
+++ src/messagingApp/Application/Features/Chats/Rules/ChatBusinessRules.cs
@@ -1,8 +1,10 @@
 ﻿using Application.Features.Auth.Constants;
+using Application.Features.Chats.Constants;
 using Application.Services.Repositories;
 using Core.Application.Security;
 using Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +13,35 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Chats.Rules;
 
-public class ChatBusinessRules(IChatRepository chatRepository)
+public class ChatBusinessRules
 {
-    public void ChatShouldExistWhenSelected(Chat? user)
+    public void ChatShouldExistWhenSelected(Chat? chat)
     {
-        if (user is null)
+        if (chat is null)
             throw new Exception("Chat not found");
     }
+
+
+    public void UserShouldExistWhenSelected(User? user)
+    {
+        if (user is null)
+            throw new Exception("User not found");
+    }
+
+
+    public void UserShouldNotBeInChatWhenJoining(ChatUser? chatUsr)
+    {
+        if (chatUsr is not null)
+            throw new Exception("User is already in this chat");
+    }
+
+
+    public void CheckUserLimitWhenJoining(Chat chat)
+    {
+        if (chat.ChatUsers.Count >= ChatConstants.MaxUsersInChat)
+            throw new Exception("Chat is full");
+    }
+
 
 
 }
